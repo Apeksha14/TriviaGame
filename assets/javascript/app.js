1a564f52607a35921b800ea1fa248e85926f75c9
Vivek Hakim <vivekhakim@Viveks-MacBook-Air.local>
2017-01-09 23:19:10 -0500

    Final Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $("#start").on("click",function(){
 
-$("#start").css("display","none");
-$("#formdiv1").css("display","block");
-$(".timediv").css("display","block");
-timeCounter.start();
+	$("#start").css("display","none");
+	$("#formdiv1").css("display","block");
+	$(".timediv").css("display","block");
+	timeCounter.start();
 
 });
 
@@ -30,12 +30,12 @@ count: function() {
      timeCounter.time--;
 	 $("#time").html(timeCounter.time);
 	 if(timeCounter.time === 0)
-	 {
+	 	{
 
-	 timeCounter.stop();
-	 timeCounter.timeOut();
+	 		timeCounter.stop();
+	 		timeCounter.timeOut();
 
-	}
+		}
 
 },
 stop: function() {
@@ -47,92 +47,92 @@ timeOut:function(){
 
 	$("#timeout").html("Time Out!!");
 	if($("#formdiv1").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: The Arc Reactor");
-		$("#formdiv1").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/aWigfwnxzqIuc.gif");
-		setTimeout(displayDiv1,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: The Arc Reactor");
+			$("#formdiv1").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/aWigfwnxzqIuc.gif");
+			setTimeout(displayDiv1,3000);
+		}
 	if($("#formdiv2").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Bucky Barnes");
-		$("#formdiv2").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/GzhrOhL0dv9jG.gif");
-		setTimeout(displayDiv2,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Bucky Barnes");
+			$("#formdiv2").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/GzhrOhL0dv9jG.gif");
+			setTimeout(displayDiv2,3000);
+		}
 	if($("#formdiv3").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Knowhere");
-		$("#formdiv3").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/AlX6UnfVVxQiI.gif");
-		setTimeout(displayDiv3,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Knowhere");
+			$("#formdiv3").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/AlX6UnfVVxQiI.gif");
+			setTimeout(displayDiv3,3000);
+		}
 	if($("#formdiv4").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Captain Marvel");
-		$("#formdiv4").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/26ybv3WJDjDb2xlIc.gif");
-		setTimeout(displayDiv4,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Captain Marvel");
+			$("#formdiv4").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/26ybv3WJDjDb2xlIc.gif");
+			setTimeout(displayDiv4,3000);
+		}
 	if($("#formdiv5").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Iron Man 2");
-		$("#formdiv5").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/8JcquOvfH3ARy.gif");
-		setTimeout(displayDiv5,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Iron Man 2");
+			$("#formdiv5").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/8JcquOvfH3ARy.gif");
+			setTimeout(displayDiv5,3000);
+		}
 	if($("#formdiv6").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was:  Matt Damon");
-		$("#formdiv6").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/WqMS5HIiaa31C.gif");
-		setTimeout(displayDiv6,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was:  Matt Damon");
+			$("#formdiv6").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/WqMS5HIiaa31C.gif");
+			setTimeout(displayDiv6,3000);
+		}
 	if($("#formdiv7").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Sif");
-		$("#formdiv7").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/Viv9wKLP1BOc8.gif");
-		setTimeout(displayDiv7,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Sif");
+			$("#formdiv7").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/Viv9wKLP1BOc8.gif");
+			setTimeout(displayDiv7,3000);
+		}
 	if($("#formdiv8").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was: Thanos");
-		$("#formdiv8").css("display","none");
+		{
+			noans++;
+			$("#noans").html("The correct answer was: Thanos");
+			$("#formdiv8").css("display","none");
 			$("#ans1").attr("src","http://i.giphy.com/TGP2OlbTirYXu.gif");
-		setTimeout(displayDiv8,3000);
-	}
+			setTimeout(displayDiv8,3000);
+		}
 	if($("#formdiv9").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was:  Black Panther");
-		$("#formdiv9").css("display","none");
-		$("#ans1").attr("src","https://media.giphy.com/media/eYFjD1LQVeTKw/source.gif");
-		setTimeout(displayDiv9,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was:  Black Panther");
+			$("#formdiv9").css("display","none");
+			$("#ans1").attr("src","https://media.giphy.com/media/eYFjD1LQVeTKw/source.gif");
+			setTimeout(displayDiv9,3000);
+		}
 	if($("#formdiv10").css("display") === "block")
-	{
-		noans++;
-		$("#noans").html("The correct answer was:  Vin Diesel");
-		$("#formdiv10").css("display","none");
-		$("#ans1").attr("src","http://i.giphy.com/znXPZJUqZLeda.gif");
-		setTimeout(displayDiv10,3000);
-	}
+		{
+			noans++;
+			$("#noans").html("The correct answer was:  Vin Diesel");
+			$("#formdiv10").css("display","none");
+			$("#ans1").attr("src","http://i.giphy.com/znXPZJUqZLeda.gif");
+			setTimeout(displayDiv10,3000);
+		}
 
 	$("#answer").css("display","block");
 
 }
 };
 
-$("#formdiv1 button").on("click",function(){
+	$("#formdiv1 button").on("click",function(){
 
 			if($(this).val() === "arc")
 				{
@@ -154,18 +154,15 @@ $("#formdiv1 button").on("click",function(){
 			$("#answer").css("display","block");
 			setTimeout(displayDiv1,3000);
 
-		});
+	});
 
-		function displayDiv1(){
-			
+	function displayDiv1()
+		{
 			reset();
 			$("#formdiv2").css("display","block");
-
 		} 
 
-
-
-		$("#formdiv2 button").on("click",function(){
+	$("#formdiv2 button").on("click",function(){
 
 			if($(this).val() === "bucky")
 				{
@@ -188,16 +185,15 @@ $("#formdiv1 button").on("click",function(){
 			$("#answer").css("display","block");
 			setTimeout(displayDiv2,3000);
 			
-		});
+	});
 
-		function displayDiv2(){
-
+	function displayDiv2()
+		{
 			reset();
 			$("#formdiv3").css("display","block");
-
 		}
 
-		$("#formdiv3 button").on("click",function(){
+	$("#formdiv3 button").on("click",function(){
 
 			if($(this).val() === "know")
 				{
@@ -222,16 +218,15 @@ $("#formdiv1 button").on("click",function(){
 			$("#answer").css("display","block");
 			setTimeout(displayDiv3,3000);
 			
-		});
+	});
 
-		function displayDiv3(){
-
+	function displayDiv3()
+		{
 			reset();
 			$("#formdiv4").css("display","block");
-
 		}
 
-$("#formdiv4 button").on("click",function(){
+	$("#formdiv4 button").on("click",function(){
 
 			if($(this).val() === "cmarvel")
 				{
@@ -255,16 +250,15 @@ $("#formdiv4 button").on("click",function(){
 			$("#answer").css("display","block");
 			setTimeout(displayDiv4,3000);
 
-		});
+	});
 
-		function displayDiv4(){
-
+	function displayDiv4()
+		{
 			reset();
 			$("#formdiv5").css("display","block");
-
 		}
 
-$("#formdiv5 button").on("click",function(){
+	$("#formdiv5 button").on("click",function(){
 
 			if($(this).val() === "ironman")
 		
@@ -288,16 +282,15 @@ $("#formdiv5 button").on("click",function(){
 			$("#ans1").attr("src","http://i.giphy.com/8JcquOvfH3ARy.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv5,3000);
-		});
+	});
 
-		function displayDiv5(){
-
+	function displayDiv5()
+		{
 			reset();
 			$("#formdiv6").css("display","block");
-
 		}
 
-$("#formdiv6 button").on("click",function(){
+	$("#formdiv6 button").on("click",function(){
 
 			if($(this).val() === "matt")
 		
@@ -321,16 +314,15 @@ $("#formdiv6 button").on("click",function(){
 			$("#ans1").attr("src","http://i.giphy.com/WqMS5HIiaa31C.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv6,3000);
-		});
+	});
 
-		function displayDiv6(){
-
+	function displayDiv6()
+		{
 			reset();
 			$("#formdiv7").css("display","block");
-
 		}
 
-$("#formdiv7 button").on("click",function(){
+	$("#formdiv7 button").on("click",function(){
 
 			if($(this).val() === "sif")
 		
@@ -354,16 +346,15 @@ $("#formdiv7 button").on("click",function(){
 			$("#ans1").attr("src","http://i.giphy.com/Viv9wKLP1BOc8.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv7,3000);
-		});
+	});
 
-		function displayDiv7(){
-
+	function displayDiv7()
+		{
 			reset();
 			$("#formdiv8").css("display","block");
-
 		}
 
-$("#formdiv8 button").on("click",function(){
+	$("#formdiv8 button").on("click",function(){
 
 			if($(this).val() === "thanos")
 		
@@ -387,16 +378,15 @@ $("#formdiv8 button").on("click",function(){
 			$("#ans1").attr("src","http://i.giphy.com/TGP2OlbTirYXu.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv8,3000);
-		});
+	});
 
-		function displayDiv8(){
-
+	function displayDiv8()
+		{
 			reset();
 			$("#formdiv9").css("display","block");
-
 		}
 
-$("#formdiv9 button").on("click",function(){
+	$("#formdiv9 button").on("click",function(){
 
 			if($(this).val() === "panther")
 		
@@ -420,16 +410,15 @@ $("#formdiv9 button").on("click",function(){
 			$("#ans1").attr("src","https://media.giphy.com/media/eYFjD1LQVeTKw/source.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv9,3000);
-		});
+	});
 
-		function displayDiv9(){
-
+	function displayDiv9()
+		{
 			reset();
 			$("#formdiv10").css("display","block");
-
 		}
 
-$("#formdiv10 button").on("click",function(){
+	$("#formdiv10 button").on("click",function(){
 
 			if($(this).val() === "vin")
 		
@@ -453,35 +442,31 @@ $("#formdiv10 button").on("click",function(){
 			$("#ans1").attr("src","http://i.giphy.com/znXPZJUqZLeda.gif");
 			$("#answer").css("display","block");
 			setTimeout(displayDiv10,3000);
-		});
+	});
 
-		function displayDiv10(){
-
-			
+	function displayDiv10()
+		{
 			$("#answer").css("display","none");
 			$("#formdiv11").css("display","block");
 			$("#correct").html(correct);
 			$("#wrong").html(wrong);
 		 	$("#unans").html(noans);
-		
 		}
 
 	$("#startover").on("click",function(){
 
 			reset();
-			correct = 0;wrong=0;noans=0;
+			correct = 0;
+			wrong=0;
+			noans=0;
 			$("#formdiv11").css("display","none");
 			$("#correct").html("");
 			$("#wrong").html("");
 			$("#unans").html("");
 			$("#formdiv1").css("display","block");
-
-
 	});
 
-
-
-	 function reset()
+	function reset()
 	 	{
 			timeCounter.reset();
 			timeCounter.start();
@@ -490,7 +475,6 @@ $("#formdiv10 button").on("click",function(){
 			$("#noans").html("");
 			$("#timeout").html("");
 			$("#answer").css("display","none");
-
 		}
 
 
